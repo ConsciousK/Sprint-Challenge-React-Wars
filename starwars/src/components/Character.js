28a1d9f779b8c9552213f35f681af99a7e0a4d29
Kevin Grant <ConsciousK@yahoo.com>
2019-06-08 13:33:57 -0400

    Completed Basic Styling of Props and Character Card.

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,14 +1,22 @@
 import React from "react";
 import './components.css';
 function Character(props) {
-  return (
-    <div className="characterBox">
-      <p className="characterItem">Name: {props.character.name}</p>
-      <p className="characterItem">Birth Year:{props.character.birth_year}</p>
-      <p className="characterItem">Eye Color: {props.character.eye_color}</p>
-      <p className="characterItem">Gender: {props.character.gender}</p>
-    </div>
-  );
+    return (
+        <div className="characterBox">
+            <p className="characterItem">
+                <h3>{props.character.name}</h3>
+            </p>
+            <p className="characterItem">
+                <strong>Birth Year:</strong><span className="prop">{props.character.birth_year}</span>
+            </p>
+            <p className="characterItem">
+                <strong>Eye Color:</strong> <span className="prop">{props.character.eye_color}</span>
+            </p>
+            <p className="characterItem">
+                <strong>Gender:</strong><span className="prop">{props.character.gender}</span>
+            </p>
+        </div>
+    );
 }
 
 export default Character;
